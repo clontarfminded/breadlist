d515277b943296672141edc1775351cb4b59a910
clontarfminded <clontarfminded@icloud.com>
2020-04-04 14:02:04 -0500

    more earlyu work

--- breadlist/classifieds/views.py
+++ breadlist/classifieds/views.py
@@ -5,11 +5,10 @@ from .models import Classified
 
 def index(request):
     latest_classified_list = Classified.objects.order_by('-datetime_created')[:5]
-    template = loader.get_template('classifieds/index.html')
     context = {
         'latest_classified_list': latest_classified_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'classifieds/index.html', context)
 
 def index_locale(request, locale_name):
     response = "hello, you're looking at the index for {}".format(locale_name)
@@ -23,6 +22,11 @@ def index_narotype(request, locale_name, section_name, narotype_name):
     response = "hello, you're looking at the index for {} of {} in {}".format(narotype_name, section_name, locale_name)
     return HttpResponse(response)
 
-def classified_detail(request):
-    response = "hello, you're looking at the detail page for {}".format(classified_title)
-    return HttpResponse(response)
+def classified_detail(request, locale_name, section_name, narotype_name, classified_id):
+    context = {
+        'locale_name': locale_name,
+        'section_name': section_name,
+        'narotype_name': narotype_name,
+        'classified_id': classified_id
+    }
+    return render(request, 'classifieds/detail.html', context)

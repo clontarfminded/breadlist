6fe988d39d1bf8abfad86d923fc2de579984efeb
clontarfminded <bill@MacBook-Pro.net>
2020-04-05 16:05:56 -0500

    trouble with links. updated templates

--- breadlist/classifieds/views.py
+++ breadlist/classifieds/views.py
@@ -1,25 +1,38 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Classified
+from .models import Classified, Locale, Section, Subsection
 
 def index(request):
-    latest_classified_list = Classified.objects.order_by('-datetime_created')[:5]
+    locale_list = Locale.objects.order_by('pk')
+    section_list = Section.objects.order_by('pk')
+    subsection_list = Subsection.objects.order_by('pk')
     context = {
-        'latest_classified_list': latest_classified_list,
+        'locale_list': locale_list,
+        'section_list': section_list,
+        'subsection_list': subsection_list
     }
     return render(request, 'classifieds/index.html', context)
 
-def index_locale(request, locale_name):
-    response = "hello, you're looking at the index for {}".format(locale_name)
+def index_locale(request, locale_id):
+    locale = get_object_or_404(Locale, pk=locale_id)
+    locale_list = Locale.objects.order_by('pk')
+    section_list = Section.objects.order_by('pk')
+    subsection_list = Subsection.objects.order_by('pk')
+    context = {
+        'locale': locale,
+        'locale_list': locale_list,
+        'section_list': section_list,
+        'subsection_list': subsection_list
+    }
+    return render(request, 'classifieds/locale-index.html', context)
+
+def index_section(request, locale_id, section_id):
+    response = "hello, you're looking at the index for {} in {}".format(section_id, locale_id)
     return HttpResponse(response)
 
-def index_section(request, locale_name, section_name):
-    response = "hello, you're looking at the index for {} in {}".format(section_name, locale_name)
-    return HttpResponse(response)
-
-def index_subsection(request, locale_name, section_name, subsection_name):
-    response = "hello, you're looking at the index for {} of {} in {}".format(subsection_name, section_name, locale_name)
+def index_subsection(request, locale_id, section_id, subsection_id):
+    response = "hello, you're looking at the index for {} of {} in {}".format(subsection_id, section_id, locale_id)
     return HttpResponse(response)
 
 def detail(request, classified_id):

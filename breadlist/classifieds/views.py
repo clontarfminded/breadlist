8a910efe81a1bde10e572b17061c12dabe96ef9d
clontarfminded <bill@MacBook-Pro.net>
2020-04-05 17:16:37 -0500

    added section index

--- breadlist/classifieds/views.py
+++ breadlist/classifieds/views.py
@@ -27,8 +27,19 @@ def index_locale(request, locale_name):
     return render(request, 'classifieds/locale-index.html', context)
 
 def index_section(request, locale_name, section_name):
-    response = "hello, you're looking at the index for {} in {}".format(section_name, locale_name)
-    return HttpResponse(response)
+    section_classified_list = Classified.objects.filter(locale__locale_name=locale_name).filter(section__section_name=section_name).order_by('datetime_created')
+    locale_list = Locale.objects.order_by('pk')
+    section_list = Section.objects.order_by('pk')
+    subsection_list = Subsection.objects.order_by('pk')
+    context = {
+        'locale_name': locale_name,
+        'section_name': section_name,
+        'locale_list': locale_list,
+        'section_list': section_list,
+        'subsection_list': subsection_list,
+        'section_classified_list': section_classified_list
+    }
+    return render(request, 'classifieds/section-index.html', context)
 
 def index_subsection(request, locale_name, section_name, subsection_name):
     response = "hello, you're looking at the index for {} of {} in {}".format(subsection_name, section_name, locale_name)

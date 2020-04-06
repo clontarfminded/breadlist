6947b81c9f26c57aed0a31e3da4d910a98df7f21
clontarfminded <bill@MacBook-Pro.net>
2020-04-05 19:30:18 -0500

    added basic footer, made everything lower case

--- breadlist/classifieds/views.py
+++ breadlist/classifieds/views.py
@@ -42,8 +42,20 @@ def index_section(request, locale_name, section_name):
     return render(request, 'classifieds/section-index.html', context)
 
 def index_subsection(request, locale_name, section_name, subsection_name):
-    response = "hello, you're looking at the index for {} of {} in {}".format(subsection_name, section_name, locale_name)
-    return HttpResponse(response)
+    subsection_classified_list = Classified.objects.filter(locale__locale_name=locale_name).filter(section__section_name=section_name).filter(subsection__subsection_name=subsection_name).order_by('datetime_created')
+    locale_list = Locale.objects.order_by('pk')
+    section_list = Section.objects.order_by('pk')
+    subsection_list = Subsection.objects.order_by('pk')
+    context = {
+        'locale_name': locale_name,
+        'section_name': section_name,
+        'subsection_name': subsection_name,
+        'locale_list': locale_list,
+        'section_list': section_list,
+        'subsection_list': subsection_list,
+        'subsection_classified_list': subsection_classified_list
+    }
+    return render(request, 'classifieds/subsection-index.html', context)
 
 def detail(request, classified_id):
     classified = get_object_or_404(Classified, pk=classified_id)

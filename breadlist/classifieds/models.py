2ab4ef4b8ffca41c1219a2adaf2c1dfe8f84f91f
clontarfminded <clontarfminded@icloud.com>
2020-04-11 10:36:28 -0500

    imported locales

--- breadlist/classifieds/models.py
+++ breadlist/classifieds/models.py
@@ -4,26 +4,20 @@ from django.utils import timezone
 class Locale(models.Model):
     locale_name = models.CharField(max_length=200)
     abbreviation = models.CharField(max_length=8)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
     region = models.CharField(max_length=200)
-    country = models.CharField(max_length=200)
-    timezone = models.CharField(max_length=200)
+    area = models.CharField(max_length=200)
     def __str__(self):
-        return self.locale_name
+        return self.abbreviation
 
 class Sublocale(models.Model):
     sublocale_name = models.CharField(max_length=200)
     abbreviation = models.CharField(max_length=8)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
     region = models.CharField(max_length=200)
-    country = models.CharField(max_length=200)
-    timezone = models.CharField(max_length=200)
+    area = models.CharField(max_length=200)
     parent_locale = models.ForeignKey('Locale', on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.sublocale_name
+        return self.abbreviation
 
 class Section(models.Model):
     section_name = models.CharField(max_length=200)

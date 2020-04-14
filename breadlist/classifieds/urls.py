4e962e676cad4b7cdd1df951c5fccc205f68c50b
clontarfminded <clontarfminded@icloud.com>
2020-04-13 20:39:25 -0500

    fixed urls got rid of doing them by abbrev

--- breadlist/classifieds/urls.py
+++ breadlist/classifieds/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('provinces/<str:province_name>', views.province, name='province'),
     path('<locale>/', views.locale_index, name='locale'),
     path('<locale>/<str:section>/', views.section_index, name='section'),
-    path('<str:locale__abbreviation>/<str:section>/<str:subsection>', views.subsection_index, name='subsection'),
-    path('<str:locale.abbreviation>/<str:section>/<str:subsection>/<int:classified__id>', views.detail, name='detail'),
+    path('<locale>/<str:section>/<str:subsection>', views.subsection_index, name='subsection'),
+    path('<locale>/<str:section>/<str:subsection>/<int:classified__id>', views.detail, name='detail'),
 
 ]

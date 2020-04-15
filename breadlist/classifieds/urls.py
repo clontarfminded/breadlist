1493f21889d0f4eb62a6c69ea5b16754b63b6bd8
clontarfminded <clontarfminded@icloud.com>
2020-04-14 21:35:14 -0500

    got a form to create a classified

--- breadlist/classifieds/urls.py
+++ breadlist/classifieds/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = "classifieds"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create-classified/', views.create_classified, name='create_classified'),
+    path('thanks/', views.thanks, name='thanks'),
     path('pages/<str:page_name>/', views.page, name='page'),
     path('regions/<str:region_name>', views.region, name='region'),
     path('provinces/<str:province_name>', views.province, name='province'),

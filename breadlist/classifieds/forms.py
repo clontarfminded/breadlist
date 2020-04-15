bfd99241cd42eef59ec10b17063b3bfcb6c7da55
clontarfminded <clontarfminded@icloud.com>
2020-04-15 13:14:09 -0500

    got rid of dupe locales

--- breadlist/classifieds/forms.py
+++ breadlist/classifieds/forms.py
@@ -4,25 +4,6 @@ from django.forms import ModelForm
 from .models import Locale, Section, Subsection
 from classifieds.models import Classified
 
-# class CreateClassifiedForm(forms.Form):
-#     LOCALECHOICES = []
-#     SECTIONCHOICES = []
-#     SUBSECTIONCHOICES = []
-#     locale_list = Locale.objects.order_by('locale_name')
-#     section_list = Section.objects.order_by('pk')
-#     subsection_list = Subsection.objects.order_by('pk')
-#     for l in locale_list:
-#         LOCALECHOICES.append((str(l), str(l)))
-#     for s in section_list:
-#         SECTIONCHOICES.append((str(s), str(s)))
-#     for ss in subsection_list:
-#         SUBSECTIONCHOICES.append((str(ss), str(ss)))
-#     classified_title = forms.CharField(max_length=200)
-#     classified_text = forms.CharField(max_length=20000)
-#     locale = forms.TypedChoiceField(choices=LOCALECHOICES)
-#     section = forms.ChoiceField(choices=SECTIONCHOICES)
-#     subsection = forms.ChoiceField(choices=SUBSECTIONCHOICES)
-
 class CreateClassifiedForm(ModelForm):
     class Meta:
         model = Classified
